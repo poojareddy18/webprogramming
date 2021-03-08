3c1ef7ac95e8696bd26dadf6874057a64a3c1c87
Pooja Reddy Gopu <60166586+poojareddy18@users.noreply.github.com>
2021-03-08 11:57:01 -0600

    Restful API

---
+++ RestfulAPI-Angular/src/app/search-recipe/search-recipe.component.ts
@@ -0,0 +1,55 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-search-recipe',
+  templateUrl: './search-recipe.component.html',
+  styleUrls: ['./search-recipe.component.css']
+})
+export class SearchRecipeComponent implements OnInit {
+  @ViewChild('recipe') recipes: ElementRef;
+  @ViewChild('place') places: ElementRef;
+  recipeValue: any;
+  placeValue: any;
+  venueList: any;
+  recipeList: any;
+
+  currentLat: any;
+  currentLong: any;
+  geolocationPosition: any;
+
+  constructor(private _http: HttpClient) {
+  }
+
+  ngOnInit() {
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.geolocationPosition = position;
+        this.currentLat = position.coords.latitude;
+        this.currentLong = position.coords.longitude;
+      });
+  }
+
+  getVenues() {
+
+    this.recipeValue = this.recipes.nativeElement.value;
+    this.placeValue = this.places.nativeElement.value;
+    if (this.recipeValue !== null) {
+      this._http.get('https://api.edamam.com/search?q=' + this.recipeValue +
+        '&app_id=8a93a66f&app_key=7564b62af9554affdda6dd1048902ac8').
+      subscribe(respReceipe => {
+        this.recipeList = respReceipe['hits'];
+      }, error => {});
+
+    }
+
+    if (this.placeValue != null && this.placeValue !== '') {
+      this._http.get('https://api.foursquare.com/v2/venues/search?client_id=HF1LSL5G4EMEQLZQU1OBVYD3IJNPK3O0YDLGTPTZXTIJQIG4' +
+        '&client_secret=G1WZ4RIDSE5CNJEKZZ5BTWGQHLUHUTTPWACSVJFPA1FVYB0F&v=20180323&near=' + this.placeValue + '&query=restaurant').
+      subscribe(respRestuarant => {
+        this.venueList = respRestuarant['response']['venues'];
+      }, error => {});
+    }
+  }
+}
